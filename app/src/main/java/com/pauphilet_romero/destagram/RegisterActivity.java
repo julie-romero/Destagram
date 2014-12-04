cd4c952857e1c188e12db9fd0d9f3bd8f6ba23f3
julie-romero <Julie.romero@utt.fr>
2014-12-04 15:51:12 +0100

    Register Layout
    Mise en forme du layout

--- app/src/main/java/com/pauphilet_romero/destagram/RegisterActivity.java
+++ app/src/main/java/com/pauphilet_romero/destagram/RegisterActivity.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class RegisterActivity extends Activity {
+
+    // booléen déterminant si une erreur est apparue lors de l'inscription
+    private Boolean error = true;
+    // champ texte de l'email
+    private EditText emailField;
+    // champ texte du mot de passe
+    private EditText passwordField;
+    // champ texte de la confirmation du mot de passe
+    private EditText passwordConfirmField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailField = (EditText) findViewById(R.id.emailField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        passwordConfirmField = (EditText) findViewById(R.id.passwordConfirmField);
     }
 
 
@@ -35,5 +50,9 @@ public class RegisterActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void register(View view) {
+
     }
 }
