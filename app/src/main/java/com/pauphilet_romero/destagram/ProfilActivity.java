90f4075ad698f2d88c291bf356f4ae668757a384
Jimmy Pauphilet <jimmy.pauphilet@gmail.com>
2014-12-09 20:03:05 +0100

    Test des onglets de la page Profil
    (échec pour le moment..)

--- app/src/main/java/com/pauphilet_romero/destagram/ProfilActivity.java
+++ app/src/main/java/com/pauphilet_romero/destagram/ProfilActivity.java
@@ -1,17 +1,56 @@
 package com.pauphilet_romero.destagram;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.pauphilet_romero.destagram.R;
+
+import com.pauphilet_romero.destagram.profileTabs.FriendFragment;
+import com.pauphilet_romero.destagram.profileTabs.HomeFragment;
+import com.pauphilet_romero.destagram.profileTabs.MediaFragment;
+import com.pauphilet_romero.destagram.profileTabs.TabListener;
 
 public class ProfilActivity extends Activity {
+
+    // Declaring our tabs and the corresponding fragments.
+    ActionBar.Tab mediaTab, homeTab, friendTab;
+    Fragment mediaFragmentTab = new MediaFragment();
+    Fragment friendFragmentTab = new FriendFragment();
+    Fragment homeFragmentTab = new HomeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil);
+        setContentView(R.layout.activity_main);
+
+        // Asking for the default ActionBar element that our platform supports.
+        ActionBar actionBar = getActionBar();
+
+        // Screen handling while hiding ActionBar icon.
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        // Screen handling while hiding Actionbar title.
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        // Creating ActionBar tabs.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Setting custom tab icons.
+        mediaTab = actionBar.newTab().setIcon(R.drawable.ic_plus);
+        friendTab = actionBar.newTab().setIcon(R.drawable.ic_friends);
+        homeTab = actionBar.newTab().setIcon(R.drawable.ic_home);
+
+        // Setting tab listeners.
+        mediaTab.setTabListener(new TabListener(mediaFragmentTab));
+        friendTab.setTabListener(new TabListener(friendFragmentTab));
+        homeTab.setTabListener(new TabListener(homeFragmentTab));
+
+        // Adding tabs to the ActionBar.
+        actionBar.addTab(mediaTab);
+        actionBar.addTab(friendTab);
+        actionBar.addTab(homeTab);
     }
 
 
